3172c8047049ac8679b66d12ebd42380f4973a20
mfregoso <mfregoso@users.noreply.github.com>
2019-04-10 23:08:31 -0700

    start testing taxpayer data

---
+++ __tests__/taxpayers.test.js
@@ -0,0 +1,26 @@
+import {TaxPayer} from "../src/utils/calculator";
+import {taxpayerStd, taxpayerItem} from "../src/constants/test_data";
+import {STD_DEDUCTION} from "../src/constants";
+
+describe('New Taxpayers', () => {
+  test('Standard: no income', () => {
+    const sTaxpayer = new TaxPayer(taxpayerStd);
+    expect(sTaxpayer).toBeInstanceOf(TaxPayer);
+  });
+
+  test('Standard taxpayer = input', () => {
+    const sTaxpayer = new TaxPayer(taxpayerStd);
+    expect(sTaxpayer.deductions).toEqual(STD_DEDUCTION);
+  });
+
+  test('Itemized: no income/deductions', () => {
+    const iTaxpayer = new TaxPayer(taxpayerItem);
+    expect(iTaxpayer).toBeInstanceOf(TaxPayer);
+  });
+
+  test('Itemized taxpayer = input', () => {
+    const iTaxpayer = new TaxPayer(taxpayerItem);
+    expect(iTaxpayer).toEqual(taxpayerItem);
+  });
+
+});
