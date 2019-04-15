3434810d3a21df441f44a8daf8a4e70bdd57d366
mfregoso <mfregoso@users.noreply.github.com>
2019-04-14 20:02:27 -0700

    test 15% bracket for both deductions

--- __tests__/taxCalculator.test.js
+++ __tests__/taxCalculator.test.js
@@ -18,40 +18,82 @@ describe('Instantiation scenarios', () => {
     expect(stdTax.deductions).toEqual(STD_DEDUCTION);
   });
   test('Itemized tax form', () => {
-    const ItemTax = new TaxCalculator(taxpayerItem);
-    expect(ItemTax).toBeInstanceOf(TaxCalculator);
+    const itemTax = new TaxCalculator(taxpayerItem);
+    expect(itemTax).toBeInstanceOf(TaxCalculator);
   });
   test('Value of deductions when itemized = input', () => {
-    const ItemTax = new TaxCalculator(taxpayerItem);
-    expect(ItemTax.deductions).toEqual(taxpayerItem.deductions);
+    const itemTax = new TaxCalculator(taxpayerItem);
+    expect(itemTax.deductions).toEqual(taxpayerItem.deductions);
   });
 });
 
-describe('STANDARD deduction scenarios', () => {
+describe('STANDARD: 15% bracket scenarios', () => {
   test('Income: 0', () => {
     const stdTax0 = new TaxCalculator(taxpayerStd);
     expect(stdTax0.getTaxLiability()).toEqual(0);
   });
 
-  test('Income: 12,500', () => {
-    const stdTax12500 = new TaxCalculator({...taxpayerStd, income: 12500});
-    expect(stdTax12500.getTaxLiability()).toEqual(0);
+  test('Income: 16,500, IRA: 0', () => {
+    const stdTax16500 = new TaxCalculator({...taxpayerStd, income: 16500});
+    expect(stdTax16500.getTaxLiability()).toEqual(0);
   });
 
-  test('Income: 12,502', () => {
-    const stdTax12502 = new TaxCalculator({...taxpayerStd, income: 12502});
-    expect(stdTax12502.getTaxLiability()).toEqual(1);
+  test('Income: 16,502, IRA: 0', () => {
+    const stdTax16502 = new TaxCalculator({...taxpayerStd, income: 16502});
+    expect(stdTax16502.getTaxLiability()).toEqual(1);
+  });
+
+  test('Income: 22500, IRA: 5500', () => {
+    const stdTax = new TaxCalculator({...taxpayerStd, income: 22500, iraContrib: 5500});
+    expect(stdTax.getTaxLiability()).toEqual(75);
+  });
+
+  test('Income: 22000, IRA: 5500', () => {
+    const stdTax = new TaxCalculator({...taxpayerStd, income: 22000, iraContrib: 5500});
+    expect(stdTax.getTaxLiability()).toEqual(0);
+  });
+
+  test('Income: 79500, IRA: 3000', () => {
+    const stdTax = new TaxCalculator({...taxpayerStd, income: 79500, iraContrib: 3000});
+    expect(stdTax.getTaxLiability()).toEqual(9000);
   });
 });
 
-describe('ITEMIZED deduction scenarios', () => {
-  test('Income: 0, Deductions: 0', () => {
-    const itmTax = new TaxCalculator(taxpayerItem);
-    expect(itmTax.validate()).toBeFalsy();
+describe('STANDARD: 20% bracket scenarios', () => {
+  test('Income: 0', () => {
+    const stdTax0 = new TaxCalculator(taxpayerStd);
+    expect(stdTax0.getTaxLiability()).toEqual(0);
   });
 
-  test('Income: 8,000, Deductions: 11,000', () => {
-    const itmTax = new TaxCalculator({...taxpayerItem, income: 8000, deductions: 11000});
+  test('Income: 16,500, IRA: 0', () => {
+    const stdTax16500 = new TaxCalculator({...taxpayerStd, income: 16500});
+    expect(stdTax16500.getTaxLiability()).toEqual(0);
+  });
+});
+
+describe('ITEMIZED: 15% bracket scenarios', () => {
+  test('Income: 0, Deductions: 0', () => {
+    const itmTax = new TaxCalculator(taxpayerItem);
+    expect(itmTax.checkForm().isValid).toBeFalsy();
+  });
+
+  test('Income: 8000, Deductions: 13000', () => {
+    const itmTax = new TaxCalculator({...taxpayerItem, income: 8000, deductions: 13000});
     expect(itmTax.getTaxLiability()).toEqual(0);
+  });
+
+  test('Income: 22000, Deductions: 12500, IRA: 5500', () => {
+    const itmTax = new TaxCalculator({...taxpayerItem, income: 22000, deductions: 12500, iraContrib: 5500});
+    expect(itmTax.getTaxLiability()).toEqual(0);
+  });
+
+  test('Income: 79500, Deductions: 12500, IRA: 3000', () => {
+    const itmTax = new TaxCalculator({...taxpayerItem, income: 79500, deductions: 12500, iraContrib: 3000});
+    expect(itmTax.getTaxLiability()).toEqual(9000);
+  });
+
+  test('Income: 75000, Deductions: 24000, IRA: 0', () => {
+    const itmTax = new TaxCalculator({...taxpayerItem, income: 75000, deductions: 24000, iraContrib: 0});
+    expect(itmTax.getTaxLiability()).toEqual(7050);
   });
 });
